7cc51bfc84feb981b7fb9f02e06bf1a428eb79ea
John <dinasty059@hotmail.fr>
2017-12-02 15:46:06 +0100

    refacto(Spring) : refacto configuration

---
+++ GotGame/src/main/java/Hello/Application.java
@@ -0,0 +1,18 @@
+package Hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("L'application démarre !");
+    }
+
+}
